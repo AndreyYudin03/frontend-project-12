e0709af2000a8d6f669c04c943799a751ce4c54a
andrey yudin <onetapandrey@gmail.com>
2024-09-15 21:58:40 +0300

    login form fix

--- frontend/src/components/Forms/LoginForm.jsx
+++ frontend/src/components/Forms/LoginForm.jsx
@@ -23,12 +23,19 @@ const LoginForm = () => {
       });
   };
 
+  const handleError = (authError) => {
+    if (authError === 401) {
+      return t('loginPage.error.401');
+    }
+    return t('loginPage.error.default');
+  };
+
   return (
     <AuthForm
       initialValues={{ username: '', password: '' }}
       onSubmit={handleSubmit}
       submitText={t('loginPage.submit')}
-      error={error && t('loginPage.error.default')}
+      error={error && handleError(error)}
     >
       <InputField label={t('loginPage.username')} name="username" type="text" />
       <InputField
