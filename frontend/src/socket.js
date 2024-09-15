bff9be25b4b14f399f03268d49dab5262c92510f
andrey yudin <onetapandrey@gmail.com>
2024-09-15 23:14:24 +0300

    websocket fix

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,8 +1,7 @@
 import { io } from 'socket.io-client';
 
 const socket = io('http://localhost:5001', {
-  autoConnect: false,
-  withCredentials: true,
+  transports: ['websocket'],
 });
 
 export default socket;
