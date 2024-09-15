c9f284459b71682649a01cea73b4f036ef8b2c92
andrey yudin <onetapandrey@gmail.com>
2024-09-15 19:24:07 +0300

    inputField fix

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import {
-  ErrorMessage,
-  useFormikContext,
-} from 'formik';
+import { ErrorMessage, useFormikContext } from 'formik';
 
 const InputField = ({ label, name, type }) => {
   const {
-    handleChange,
-    handleBlur,
-    touched,
-    errors,
+    handleChange, handleBlur, touched, errors,
   } = useFormikContext();
 
   return (
     <div className="input-field-wrapper">
       <div className="input-field-container">
         <input
+          id={name}
           name={name}
           type={type}
           className={`form-control ${
-            touched[name] && errors[name]
-              ? 'is-invalid'
-              : ''
+            touched[name] && errors[name] ? 'is-invalid' : ''
           }`}
           onChange={handleChange}
           onBlur={handleBlur}
-          placeholder={label} // Используем текст метки как placeholder
         />
-        {/* Удаляем label, так как он теперь не нужен */}
+        <label
+          htmlFor={name}
+          className={`input-label ${
+            touched[name] || handleChange ? 'shrunk' : ''
+          }`}
+        >
+          {label}
+        </label>
         <ErrorMessage
           component="div"
           name={name}
