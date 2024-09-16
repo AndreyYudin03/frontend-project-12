ad0a36d40b4c74c52952ad49cbe341a9ebc15cd9
andrey yudin <onetapandrey@gmail.com>
2024-09-16 15:47:50 +0300

    socket fix

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 const port = process.env.SOCKET_PORT || 5001;
-const socket = io(`http://localhost:${port}`, {
+const socket = io(`wss://localhost:${port}`, {
   autoConnect: false,
   transports: ['websocket'],
 });
